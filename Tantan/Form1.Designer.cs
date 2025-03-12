c731be5443b59f056136e5fd888a41a9c763f2ad
Tan20114 <156587199+Tan20114@users.noreply.github.com>
2025-03-13 00:11:51 +0700

    tantan commit

--- Tantan/Form1.Designer.cs
+++ Tantan/Form1.Designer.cs
@@ -29,12 +29,68 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(346, 153);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 40;
+            this.timer1.Tick += new System.EventHandler(this.Update);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(183, 209);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(522, 209);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "label3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Start);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.HandleInput);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- Tantan/Form1.cs
+++ Tantan/Form1.cs
@@ -12,9 +12,87 @@ namespace Tantan
 {
     public partial class Form1 : Form
     {
+        float score = 0;
+
+        public KeyEventArgs key;
+
+        bool p1FirstKey = false;
+        bool p2FirstKey = false;
+
+        bool p1Debuff = false;
+
         public Form1()
         {
             InitializeComponent();
+        }
+        private void Start(object sender, EventArgs e)
+        {
+            this.KeyPreview = true;
+            this.KeyDown += new KeyEventHandler(HandleInput);
+        }
+
+        private void Update(object sender, EventArgs e)
+        {
+            label1.Text = score.ToString();
+        }
+
+        private void HandleInput(object sender, KeyEventArgs e)
+        {
+            if(!p1Debuff)
+            {
+                if (e.KeyCode == Keys.A && !p1FirstKey)
+                {
+                    p1FirstKey = true;
+                    label2.Text = "A";
+                }
+                if (e.KeyCode == Keys.D && p1FirstKey)
+                {
+                    label2.Text = "D";
+                    score++;
+                    p1FirstKey = false;
+                }
+            }
+            
+            if(!p1Debuff)
+            {
+                if (e.KeyCode == Keys.J && !p2FirstKey)
+                {
+                    p2FirstKey = true;
+                    label3.Text = "J";
+                }
+                if (e.KeyCode == Keys.L && p2FirstKey)
+                {
+                    label3.Text = "L";
+                    score--;
+                    p2FirstKey = false;
+                }
+            }
+            else
+            {
+                if (e.KeyCode == Keys.J && !p2FirstKey)
+                {
+                    p2FirstKey = true;
+                    label3.Text = "J";
+                }
+                if (e.KeyCode == Keys.L && p2FirstKey)
+                {
+                    label3.Text = "L";
+                    score -= 0.1f;
+                    p2FirstKey = false;
+                }
+            }
+           
+            if(e.KeyCode == Keys.R)
+            {
+                p1Debuff = true;
+                DebuffCountdown();
+            }
+        }
+
+        async void DebuffCountdown()
+        {
+            await Task.Delay(500);
+            p1Debuff = false;
         }
     }
 }
